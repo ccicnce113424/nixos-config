68de7096230361ba93f0096f7e30829beadf7acc
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-13 00:31:22 +0800

    Trying to simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -26,90 +26,29 @@
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }: 
-  let
-    # List of hosts
-    hosts = {
-      test-vbox = {
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      test-vbox = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
         system = "x86_64-linux";
-        profile = "desktop";
-        env = "plasma";
-        users = [ "ccicnce113424" ];
-      };
-    };
-
-    # Configuration of Nix and Flake
-    nixConfigModules = [
-      ./nix-config.nix
-    ];
-
-    # Configuration of system
-    systemModules = system: [
-      ./system/${system}
-    ];
-
-    # Configuration of profile
-    profileModules = system: profile: [
-      ./profile/${system}/${profile}
-    ];
-
-    envModules = env: [
-      ./env/${env}
-    ];
-
-    # Configuration of host
-    hostModules = hostname: [
-      ({ config, pkgs, ... }: { networking.hostName = hostname; })
-      ./hosts/${hostname}
-    ];
-
-    # Configuration of users
-    userModules = users: [
-      ({ config, pkgs, ... }: {
-        users.users = builtins.listToAttrs (map (username: {
-          name = username;
-          value = { home = "/home/${username}"; };
-        }) users);
-      })
-      (map (username: ./users/${username}) users)
-    ];
-
-    # Configuration of Home Manager
-    homeManagerModules = users: specialArgs: [
-      home-manager.nixosModules.home-manager {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = specialArgs;
-        users = builtins.listToAttrs (map (username: {
-          name = username;
-          value = import ./home/${username};
-        }) users);
-      }
-    ];
-  in
-  {
-    nixosConfigurations = builtins.listToAttrs (map (hostname: 
-    let
-      specialArgs = {
-        inherit inputs;
-        host = hosts.${hostname};
-      };
-    in
-    {
-      name = hostname;
-      value = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        system = hosts.${hostname}.system;
-        modules = nixpkgs.lib.lists.flatten [
-          nixConfigModules
-          (systemModules (hosts.${hostname}.system))
-          (profileModules (hosts.${hostname}.system) (hosts.${hostname}.profile))
-          (envModules (hosts.${hostname}.env))
-          (hostModules hostname)
-          (userModules (hosts.${hostname}.users))
-          (homeManagerModules (hosts.${hostname}.users) specialArgs)
+        modules = [
+          ./nix-config.nix
+          ./system/x86_64-linux
+          ./profile/x86_64-linux/desktop
+          ./env/plasma
+          { config, pkgs, ... }: { networking.hostName = "test-vbox"; }
+          ./hosts/test-vbox
+          ./users/ccicnce113424
+          home-manager.nixosModules.home-manager {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {
+              inherit inputs;
+            };
+            users.ccicnce113424 = import ./home/ccicnce113424;
+          }
         ];
       };
-    }) (builtins.attrNames hosts));
+    }
   };
 }
