2d1a08915347225e41800f3f9051c428bcb65314
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-14 13:42:02 +0800

    Move pipewire config

--- profile/x86_64-linux/desktop/audio.nix
+++ profile/x86_64-linux/desktop/audio.nix
@@ -12,15 +12,5 @@
     pulse.enable = true;
     jack.enable = true;
 
-    extraConfig.pipewire = {
-      "10-no-resample-and-low-quantum" = {
-        "context.properties" = {
-          "default.clock.allowed-rates" = [ 44100 48000 96000 192000 ];
-          "default.clock.min-quantum" = 16;
-          "default.clock.max-quantum" = 256;
-          "default.clock.quantum" = 64;
-        };
-      };
-    };
   };
 }
