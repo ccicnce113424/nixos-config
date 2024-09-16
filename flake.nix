ae3b15ec2f81ca4d1aaf0bb1621a99fd89dcfb35
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-16 10:24:17 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,6 @@
 {
   description = "My NixOS configuration";
 
-  # Cache
-  nixConfig = {
-    substituters = [
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-    ];
-    extra-substituters = [
-     "https://nix-community.cachix.org"
-     "https://cache.nixos.org"
-   ];
-    extra-trusted-public-keys = [
-     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-   ];
-  };
-
   # Sources
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
