a40794b55dabaa5dbe5732f253a0af2271250dd5
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-16 14:21:41 +0800

    Update vscode.nix

--- home/ccicnce113424/development/vscode.nix
+++ home/ccicnce113424/development/vscode.nix
@@ -5,12 +5,16 @@
     enable = true;
     extensions = [
       pkgs.vscode-extensions.ms-ceintl.vscode-language-pack-zh-hans
-      pkgs.vscode-extensions.github.copilot
       pkgs.vscode-extensions.ms-vscode.cpptools-extension-pack
     ];
     userSettings = {
       "files.autoGuessEncoding" = true;
       "security.workspace.trust.enabled" = false;
     };
+  };
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
   };
 }
