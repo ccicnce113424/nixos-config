84d8bc6964df1005ac1c87d08fce13188a16871e
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-17 22:13:05 +0800

    Add dicts

--- env/plasma/fcitx5.nix
+++ env/plasma/fcitx5.nix
@@ -1,11 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5 = {
       plasma6Support = true;
-      addons = with pkgs; [ qt6Packages.fcitx5-chinese-addons ];
+      addons =
+        (with pkgs; [ qt6Packages.fcitx5-chinese-addons ])
+        ++ (with config.nur.repos.aleksana; [
+          fcitx5-pinyin-cedict
+          fcitx5-pinyin-chinese-idiom
+          fcitx5-pinyin-moegirl
+          fcitx5-pinyin-zhwiki
+        ]);
     };
   };
 }
--- nix-config.nix
+++ nix-config.nix
@@ -9,12 +9,12 @@
     experimental-features = "nix-command flakes";
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
-      # "https://nix-community.cachix.org"
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
   system.stateVersion = "24.05";
