0d9f606bfd7197fa5b0ed86537da923497667137
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-22 20:55:16 +0800

    add key

--- livecd/flake.nix
+++ livecd/flake.nix
@@ -34,6 +34,7 @@
                 ];
                 extra-trusted-public-keys = [
                   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                  "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
                 ];
               };
               environment.systemPackages = [ pkgs.git ];
