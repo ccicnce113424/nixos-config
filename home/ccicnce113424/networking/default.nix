9833edee290ba95930f5f30ac90700af7931ba65
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-22 13:12:05 +0800

    better firefox

--- home/ccicnce113424/networking/default.nix
+++ home/ccicnce113424/networking/default.nix
@@ -1,4 +1,20 @@
-{ ... }:
+{ config, ... }:
 {
   programs.chromium.enable = true;
+  programs.firefox.enable = true;
+  programs.firefox.profiles = {
+    default = {
+      isDefault = true;
+      settings = {
+        "intl.locale.requested" = "zh-cn";
+        "extensions.pocket.enabled" = false;
+        "extensions.autoDisableScopes" = 0;
+      };
+      extensions = with config.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+        violentmonkey
+        smartproxy
+      ];
+    };
+  };
 }
