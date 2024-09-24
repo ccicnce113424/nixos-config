0a5cafe04a89b819945758f5e0c3069ad2ea5238
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-24 19:40:13 +0800

    enable plasma panel

--- home/ccicnce113424/plasma/default.nix
+++ home/ccicnce113424/plasma/default.nix
@@ -1,6 +1,8 @@
 { ... }:
 {
   programs.plasma.enable = true;
+  # programs.plasma.overrideConfig = true;
   programs.plasma.workspace.colorScheme = "BreezeDark";
+  programs.plasma.panels = [ { floating = true; } ];
   imports = [ ./power.nix ];
 }
