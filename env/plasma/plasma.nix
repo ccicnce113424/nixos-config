a78b83d2b270f8f7c51e30f13999a3c7604320e5
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-24 20:38:40 +0800

    better plasma

--- env/plasma/plasma.nix
+++ env/plasma/plasma.nix
@@ -4,6 +4,7 @@
   services.desktopManager.plasma6.enable = true;
   environment.systemPackages = [
     (pkgs.callPackage ./vulkan-hdr-layer.nix { })
+    pkgs.kdePackages.kdeconnect-kde
   ];
   environment.sessionVariables = {
     ENABLE_HDR_WSI = "1";
