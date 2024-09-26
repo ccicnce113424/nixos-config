9465fa5cd7bccbfaa11959e692787f38514a038c
ccicnce113424 <ccicnce113424@gmail.com>
2024-09-27 01:33:44 +0800

    update hash

--- home/ccicnce113424/wine.nix
+++ home/ccicnce113424/wine.nix
@@ -8,6 +8,7 @@
       finalAttrs: previousAttrs: {
         patches = pkgs.fetchpatch {
           url = "https://gitlab.com/Ph42oN/dxvk-gplasync/-/raw/main/patches/dxvk-gplasync-master.patch";
+          hash = "sha256-boalvYY6kBNGqA4kPzck2OAiamTa1lDrWDUVtJGQij8=";
         };
       }
     ))
