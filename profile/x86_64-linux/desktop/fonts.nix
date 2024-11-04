8185bf36f206d2f2bf4fd57cfe0fc25c6400eda9
ccicnce113424 <ccicnce113424@gmail.com>
2024-11-04 13:15:38 +0800

    fix font

--- profile/x86_64-linux/desktop/fonts.nix
+++ profile/x86_64-linux/desktop/fonts.nix
@@ -6,7 +6,8 @@
     fontDir.enable = true;
     packages = with pkgs; [
       noto-fonts
-      noto-fonts-cjk
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       noto-fonts-color-emoji
       noto-fonts-monochrome-emoji
       dejavu_fonts
