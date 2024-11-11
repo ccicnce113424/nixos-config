67a854bba086b0d7b666ae28747a92fd98d15d72
ccicnce113424 <ccicnce113424@gmail.com>
2024-11-12 00:58:05 +0800

    add lutris and heroic games launcher

--- home/ccicnce113424/gaming/default.nix
+++ home/ccicnce113424/gaming/default.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  home.packages = with pkgs; [
-    heroic
-    gogdl
+  services.flatpak.packages = [
+    "net.lutris.Lutris"
+    "com.heroicgameslauncher.hgl"
   ];
 }
