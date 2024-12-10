aafd84dcf2eca7f4b90f192f83ef693111ba4595
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-10 20:54:30 +0800

    deduplicate input

--- flake.nix
+++ flake.nix
@@ -13,13 +13,18 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs."flake-parts".follows = "flake-parts";
     };
     daeuniverse = {
       url = "github:daeuniverse/flake.nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs."flake-parts".follows = "flake-parts";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     macronova = {
