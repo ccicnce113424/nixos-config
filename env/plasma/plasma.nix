002f8ed4a09f49e8aadaee3c2746047d33afd3a2
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-11 00:17:16 +0800

    smart monitoring

--- env/plasma/plasma.nix
+++ env/plasma/plasma.nix
@@ -6,6 +6,7 @@
     pkgs.nur.repos.xddxdd.vk-hdr-layer
     pkgs.kdePackages.kdeconnect-kde
     pkgs.dmidecode
+    pkgs.kdePackages.plasma-disks
   ];
   # environment.sessionVariables = {
   #   ENABLE_HDR_WSI = "1";
