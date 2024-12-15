7769dd5e96cb8f374d888e410637cfbe17494b98
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-16 00:38:56 +0800

    patch dxvk

--- profile/x86_64-linux/desktop/wine.nix
+++ profile/x86_64-linux/desktop/wine.nix
@@ -3,16 +3,22 @@
   environment.systemPackages = with pkgs; [
     wineWowPackages.stagingFull
     winetricks
-    (dxvk_2.overrideAttrs (
-      finalAttrs: previousAttrs: {
-        patches = [
-          (fetchpatch {
-            url = "https://gitlab.com/Ph42oN/dxvk-gplasync/-/raw/main/patches/dxvk-gplasync-2.5.1-2.patch";
-            hash = "sha256-cfMxLhz+SiLD7p7rdD/tI83hiLZxYjNgVl2034UlwDI=";
-          })
-        ];
-      }
-    ))
+    dxvk
     vkd3d-proton
+  ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      dxvk_2 = super.dxvk_2.overrideAttrs (
+        finalAttrs: previousAttrs: {
+          patches = [
+            (super.fetchpatch {
+              url = "https://gitlab.com/Ph42oN/dxvk-gplasync/-/raw/main/patches/dxvk-gplasync-2.5.1-2.patch";
+              hash = "sha256-cfMxLhz+SiLD7p7rdD/tI83hiLZxYjNgVl2034UlwDI=";
+            })
+          ];
+        }
+      );
+    })
   ];
 }
