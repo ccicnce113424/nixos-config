83f5cdc918fa6e973e4174256d205efb05556224
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-16 01:07:15 +0800

    remove some libs

--- profile/x86_64-linux/desktop/wine.nix
+++ profile/x86_64-linux/desktop/wine.nix
@@ -3,22 +3,5 @@
   environment.systemPackages = with pkgs; [
     wineWowPackages.stagingFull
     winetricks
-    dxvk
-    vkd3d-proton
-  ];
-
-  nixpkgs.overlays = [
-    (self: super: {
-      dxvk_2 = super.dxvk_2.overrideAttrs (
-        finalAttrs: previousAttrs: {
-          patches = [
-            (super.fetchpatch {
-              url = "https://gitlab.com/Ph42oN/dxvk-gplasync/-/raw/main/patches/dxvk-gplasync-2.5.1-2.patch";
-              hash = "sha256-cfMxLhz+SiLD7p7rdD/tI83hiLZxYjNgVl2034UlwDI=";
-            })
-          ];
-        }
-      );
-    })
   ];
 }
