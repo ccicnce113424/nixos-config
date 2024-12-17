faee7d307ff21e7567f7e0296af89be471a9a4ef
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-17 09:20:47 +0800

    install onlyoffice as flatpak package

--- home/ccicnce113424/documents.nix
+++ home/ccicnce113424/documents.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = [
-    pkgs.onlyoffice-bin
     pkgs.libreoffice-qt6-fresh
     pkgs.hunspell
     pkgs.hunspellDicts.en_US
   ];
+  services.flatpak.packages = [ "org.onlyoffice.desktopeditors" ];
 }
