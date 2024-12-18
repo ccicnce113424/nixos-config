026f4ee609bba414418be29209f45f4d0408104a
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-19 01:25:56 +0800

    rocm is not avaliable on 32 bit environment

--- hosts/modules/amdcl.nix
+++ hosts/modules/amdcl.nix
@@ -6,12 +6,11 @@
       rocmPackages.clr.icd
     ];
     extraPackages32 = with pkgs.pkgsi686Linux; [
-      # mesa.opencl
-      rocmPackages.clr.icd
+      mesa.opencl
     ];
   };
-  # environment.sessionVariables = {
-  #   RUSTICL_ENABLE = "radeonsi";
-  #   RUSTICL_FEATURES = "fp16,fp64";
-  # };
+  environment.sessionVariables = {
+    RUSTICL_ENABLE = "radeonsi";
+    RUSTICL_FEATURES = "fp16,fp64";
+  };
 }
