1cb11df8e7b4ce40bd489774e28f608d8bcc0165
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-18 23:26:05 +0800

    vscode: do not set usersettings in nix

--- home/ccicnce113424/development/vscode.nix
+++ home/ccicnce113424/development/vscode.nix
@@ -7,13 +7,13 @@
       pkgs.vscode-extensions.ms-ceintl.vscode-language-pack-zh-hans
       pkgs.vscode-extensions.github.vscode-github-actions
     ];
-    userSettings = {
-      "files.autoGuessEncoding" = true;
-      "security.workspace.trust.enabled" = false;
-    };
+    # userSettings = {
+    #   "files.autoGuessEncoding" = true;
+    #   "security.workspace.trust.enabled" = false;
+    # };
   };
-  home.file.".vscode/argv.json".text = builtins.toJSON {
-    "enable-crash-reporter" = false;
-    "locale" = "zh-cn";
-  };
+  # home.file.".vscode/argv.json".text = builtins.toJSON {
+  #   "enable-crash-reporter" = false;
+  #   "locale" = "zh-cn";
+  # };
 }
