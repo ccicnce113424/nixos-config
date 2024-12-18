a229aba680c76ad1ab1fd37599c9af9f1b2cab58
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-18 23:24:45 +0800

    pipewire: add allowed rates and change clock quantum

--- hosts/modules/better-pipewire.nix
+++ hosts/modules/better-pipewire.nix
@@ -5,13 +5,16 @@
     "10-no-resample-and-low-quantum" = {
       "context.properties" = {
         "default.clock.allowed-rates" = [
+          32000
           44100
           48000
+          88200
           96000
+          176400
           192000
         ];
-        "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 512;
+        "default.clock.min-quantum" = 16;
+        "default.clock.max-quantum" = 1024;
         "default.clock.quantum" = 128;
       };
     };
