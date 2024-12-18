ae04a4959c88e295eb5a399e88dc3c88e7b6668f
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-19 03:01:12 +0800

    adjust code

--- profile/x86_64-linux/desktop/networking.nix
+++ profile/x86_64-linux/desktop/networking.nix
@@ -17,4 +17,14 @@
       userServices = true;
     };
   };
+
+  services.daed = {
+    enable = true;
+    openFirewall = {
+      enable = true;
+      port = 12345;
+    };
+    configDir = "/etc/daed";
+    listen = "127.0.0.1:2023";
+  };
 }
