f95195f8ebe88b92107d6e0e05812e76c7b6eb64
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-19 02:02:39 +0800

    move default printing settings to profile

--- hosts/ccic-desktop/default.nix
+++ hosts/ccic-desktop/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ../modules/better-pipewire.nix
     ../modules/locale-time-cn.nix
-    ../modules/printing.nix
     ../modules/bluetooth.nix
     ../modules/tpm.nix
     ../modules/nvidia.nix
