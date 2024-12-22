2b47337e84b9b556cda75bf20353859d61c49082
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-22 17:16:32 +0800

    fix mistype

--- env/modules/firefox.nix
+++ env/modules/firefox.nix
@@ -5,7 +5,7 @@
     languagePacks = [ "zh-CN" ];
     preferences = {
       "widget.use-xdg-desktop-portal.file-picker" = 1;
-      "widget.use-xdg-desktop-portal.mine-handler" = 1;
+      "widget.use-xdg-desktop-portal.mime-handler" = 1;
       "widget.use-xdg-desktop-portal.settings" = 1;
       "widget.use-xdg-desktop-portal.location" = 1;
       "widget.use-xdg-desktop-portal.open-uri" = 1;
