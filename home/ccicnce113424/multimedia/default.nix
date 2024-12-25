757d61906d79c8e5d38e5f459b183eeff8fcf2a4
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-26 01:59:40 +0800

    modify mpv

--- home/ccicnce113424/multimedia/default.nix
+++ home/ccicnce113424/multimedia/default.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    mpv
     vlc
     netease-cloud-music-gtk
     waylyrics
     gimp
   ];
+
+  programs.mpv = {
+    enable = true;
+    config = {
+      vo = "gpu-next";
+      target-colorspace-hint = true;
+      gpu-api = "vulkan";
+      gpu-context = "waylandvk";
+      hwdec = "auto-safe";
+      scale = "ewa_lanczos4sharpest";
+      video-sync = "display-tempo";
+      interpolation = true;
+      ao = "pipewire";
+      sub-auto = "fuzzy";
+    };
+  };
 }
