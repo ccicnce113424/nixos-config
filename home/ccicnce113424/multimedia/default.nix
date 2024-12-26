e34334af673126dbcc9235dbb2282cacbd395d7b
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-26 19:05:22 +0800

    mpv: add scripts

--- home/ccicnce113424/multimedia/default.nix
+++ home/ccicnce113424/multimedia/default.nix
@@ -7,21 +7,5 @@
     gimp
   ];
 
-  programs.mpv = {
-    enable = true;
-    config = {
-      vo = "gpu-next";
-      target-colorspace-hint = true;
-      icc-profile-auto = true;
-      gpu-api = "vulkan";
-      gpu-context = "waylandvk";
-      hwdec = "auto-safe";
-      scale = "ewa_lanczos4sharpest";
-      dscale = "mitchell";
-      dither = "error-diffusion";
-      ao = "pipewire";
-      audio-file-auto = "fuzzy";
-      sub-auto = "fuzzy";
-    };
-  };
+  imports = [ ./mpv.nix ];
 }
