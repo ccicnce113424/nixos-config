5fbce0750ed017da5c4af6dddabdd6fba694f26b
ccicnce113424 <ccicnce113424@gmail.com>
2024-12-26 12:21:18 +0800

    mpv: change settings

--- home/ccicnce113424/multimedia/default.nix
+++ home/ccicnce113424/multimedia/default.nix
@@ -12,13 +12,15 @@
     config = {
       vo = "gpu-next";
       target-colorspace-hint = true;
+      icc-profile-auto = true;
       gpu-api = "vulkan";
       gpu-context = "waylandvk";
       hwdec = "auto-safe";
       scale = "ewa_lanczos4sharpest";
-      video-sync = "display-tempo";
-      interpolation = true;
+      dscale = "mitchell";
+      dither = "error-diffusion";
       ao = "pipewire";
+      audio-file-auto = "fuzzy";
       sub-auto = "fuzzy";
     };
   };
