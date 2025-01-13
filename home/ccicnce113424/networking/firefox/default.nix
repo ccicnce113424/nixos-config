bc6eecdf961e4b2803842b69c854ae09329e1237
ccicnce113424 <ccicnce113424@gmail.com>
2025-01-14 00:51:33 +0800

    firefox: add lots of plugins

--- home/ccicnce113424/networking/firefox/default.nix
+++ home/ccicnce113424/networking/firefox/default.nix
@@ -10,11 +10,14 @@
           "intl.locale.requested" = "zh-cn";
           "extensions.pocket.enabled" = false;
           "extensions.autoDisableScopes" = 0;
+          "browser.tabs.closeTabByDblclick" = true;
+          "browser.tabs.drawInTitlebar" = false;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         };
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           violentmonkey
-          foxyproxy-standard
+          smartproxy
           user-agent-string-switcher
           plasma-integration
           bilisponsorblock
@@ -22,7 +25,24 @@
           aria2-integration
           auto-tab-discard
           immersive-translate
+          gesturefy
+          scroll_anywhere
+          search-by-image
+          buster-captcha-solver
+          clearcache
+          clearurls
+          video-downloadhelper
+
+          tree-style-tab
+          tst-more-tree-commands
+          tst-tab-search
+          multiple-tab-handler
+          move-unloaded-tabs-for-tst
         ];
+
+        # https://github.com/piroor/treestyletab/wiki/Code-snippets-for-custom-style-rules#full-auto-showhide-theme
+        # Need manually set tst style and set close tab by dbclick
+        userChrome = builtins.readFile ./userChrome.css;
       };
     };
   };
