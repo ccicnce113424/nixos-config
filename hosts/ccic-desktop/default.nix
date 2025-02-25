c936c3a196241fbb6e9d94ea60d47b9d12fa0924
ccicnce113424 <ccicnce113424@gmail.com>
2025-02-26 00:30:44 +0800

    temporaily use nouveau

--- hosts/ccic-desktop/default.nix
+++ hosts/ccic-desktop/default.nix
@@ -1,6 +1,6 @@
 import ../templates/desktop.nix [
-  ../modules/nvidia.nix
-  #    ../modules/nouveau.nix
+  # ../modules/nvidia.nix
+  ../modules/nouveau.nix
   ../modules/icpu.nix
   ./hardware-configuration.nix
 ]
