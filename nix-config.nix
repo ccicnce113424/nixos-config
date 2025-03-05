ea162e88687177f02c6632fec7c60a6ebcd4c1b3
ccicnce113424 <ccicnce113424@gmail.com>
2025-03-06 01:18:26 +0800

    disable sjtu substituter

--- nix-config.nix
+++ nix-config.nix
@@ -8,7 +8,7 @@
     experimental-features = "nix-command flakes";
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
