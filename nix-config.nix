e825eefa8ae43002ce218d567d0be77b67c7af49
ccicnce113424 <ccicnce113424@gmail.com>
2025-03-05 13:05:04 +0800

    use tuna substituter

--- nix-config.nix
+++ nix-config.nix
@@ -7,7 +7,7 @@
   nix.settings = {
     experimental-features = "nix-command flakes";
     substituters = [
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
