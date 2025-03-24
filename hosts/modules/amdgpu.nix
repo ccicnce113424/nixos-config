e78e96e9adc23d26cf9051ef120619ae66ac64b0
ccicnce113424 <ccicnce113424@gmail.com>
2025-03-24 17:30:27 +0800

    amdgpu: use amdvlk

--- hosts/modules/amdgpu.nix
+++ hosts/modules/amdgpu.nix
@@ -1,6 +1,13 @@
 { ... }:
 {
-  hardware.amdgpu.opencl.enable = true;
+  hardware.amdgpu = {
+    opencl.enable = true;
+    amdvlk = {
+      enable = true;
+      support32Bit.enable = true;
+      supportExperimental.enable = true;
+    };
+  };
   environment.variables = {
     AMD_DEBUG = "useaco";
   };
