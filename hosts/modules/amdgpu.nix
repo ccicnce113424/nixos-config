8926e90d9f5090b31a9382205a5105953f9b8927
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-07 21:56:30 +0800

    remove amdvlk

--- hosts/modules/amdgpu.nix
+++ hosts/modules/amdgpu.nix
@@ -2,11 +2,6 @@
 {
   hardware.amdgpu = {
     opencl.enable = true;
-    amdvlk = {
-      enable = true;
-      support32Bit.enable = true;
-      supportExperimental.enable = true;
-    };
   };
   environment.variables = {
     AMD_DEBUG = "useaco";
