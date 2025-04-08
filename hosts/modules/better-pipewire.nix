59838a039766f94e93f4b66870a97d38b8d6ba5f
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-08 20:38:46 +0800

    set allowed pipewire rate

--- hosts/modules/better-pipewire.nix
+++ hosts/modules/better-pipewire.nix
@@ -4,13 +4,14 @@
     "10-no-resample-and-low-quantum" = {
       "context.properties" = {
         "default.clock.allowed-rates" = [
+          8000
+          16000
           32000
           44100
           48000
-          88200
           96000
-          176400
           192000
+          384000
         ];
         "default.clock.min-quantum" = 16;
         "default.clock.max-quantum" = 256;
