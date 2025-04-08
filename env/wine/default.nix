466c7ad6d3a76f7f441eb14cd2b0a58b29420eca
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-08 22:32:06 +0800

    add gstreamer libs for wine

--- env/wine/default.nix
+++ env/wine/default.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    wineWowPackages.stagingFull
-    winetricks
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      wineWowPackages.stagingFull
+      winetricks
+    ]
+    ++ (with pkgs.gst_all_1; [
+      gstreamer
+      gst-vaapi
+      gst-plugins-base
+      gst-plugins-good
+      gst-plugins-ugly
+      gst-libav
+    ]);
 }
