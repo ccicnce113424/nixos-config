84295e456c33e0b8bda0bea15fdc3b14ca8733be
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-09 00:41:01 +0800

    wine: full package already adds gst

--- env/wine/default.nix
+++ env/wine/default.nix
@@ -5,13 +5,5 @@
     [
       wineWowPackages.stagingFull
       winetricks
-    ]
-    ++ (with pkgs.gst_all_1; [
-      gstreamer
-      gst-vaapi
-      gst-plugins-base
-      gst-plugins-good
-      gst-plugins-ugly
-      gst-libav
-    ]);
+    ];
 }
