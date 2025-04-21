88aca30fd0058814ecb2f2c37b49f7601303c637
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-22 01:58:51 +0800

    use wemeet from nixpkgs

--- home/ccicnce113424/networking/default.nix
+++ home/ccicnce113424/networking/default.nix
@@ -3,7 +3,7 @@
   home.packages = [
     pkgs.qq
     pkgs.wechat-uos
-    pkgs.nur.repos.novel2430.wemeet-bin-bwrap-wayland-screenshare
+    pkgs.wemeet
     pkgs.telegram-desktop
     pkgs.tor-browser
     pkgs.qbittorrent-enhanced
