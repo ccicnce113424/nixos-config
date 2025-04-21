0cefd5f777fdb70df363ed7c00f2533d446e4a15
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-22 00:27:13 +0800

    plasma: dark theme

--- home/ccicnce113424/plasma/default.nix
+++ home/ccicnce113424/plasma/default.nix
@@ -2,6 +2,7 @@
 {
   programs.plasma.enable = true;
   # programs.plasma.overrideConfig = true;
+  programs.plasma.workspace.theme = "breeze-dark";
   programs.plasma.workspace.colorScheme = "BreezeDark";
   programs.plasma.panels = [ { floating = true; } ];
   programs.plasma.input.keyboard.numlockOnStartup = "on";
