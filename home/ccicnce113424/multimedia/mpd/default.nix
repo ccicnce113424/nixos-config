010c2a47f305531d30b6654cc1e85d8e83da94ef
ccicnce113424 <ccicnce113424@gmail.com>
2025-04-23 20:31:30 +0800

    set mixer for dop

--- home/ccicnce113424/multimedia/mpd/default.nix
+++ home/ccicnce113424/multimedia/mpd/default.nix
@@ -4,14 +4,16 @@
     enable = true;
     extraConfig = ''
       audio_output {
-        type    "pipewire"
-        name    "PipeWire Output"
+        type          "pipewire"
+        name          "PipeWire Output"
       }
       audio_output {
-        type    "alsa"
-        name    "ALSA JA11 DoP Output"
-        device  "hw:CARD=JA11"
-        dop     "yes"
+        type          "alsa"
+        name          "ALSA JA11 DoP Output"
+        device        "hw:CARD=JA11"
+        mixer_device  "pipewire"
+        mixer_control "Master"
+        dop           "yes"
       }
     '';
   };
