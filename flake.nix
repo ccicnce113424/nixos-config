e3e234503e248bd23b4658be2e215d1b8cb74cca
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-03 13:07:12 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -70,78 +70,79 @@
         };
 
       # Configuration of Nix and Flake
-      nixConfigModules = [
-        ./nix-config.nix
-      ];
+      nixConfigModules = [ ./nix-config.nix ];
 
       # Configuration of system
-      systemModules =
-        system:
-        nixpkgs.lib.optional (system != null) [
-          ./system/${system}
-        ];
+      systemModules = system: if null == system then [ ] else [ ./system/${system} ];
 
       # Configuration of profile
       profileModules =
         system: profile:
-        nixpkgs.lib.optional (system != null && profile != null) [
-          ./profile/${system}/${profile}
-        ];
+        if null == system || null == profile then [ ] else [ ./profile/${system}/${profile} ];
 
-      envModules = env: nixpkgs.lib.optional (env != null) (map (env: ./env/${env}) env);
+      envModules = env: if null == env then [ ] else map (env: ./env/${env}) env;
 
       # Configuration of host
       hostModules =
         hostname:
-        nixpkgs.lib.optional (hostname != null) [
-          (
-            { ... }:
-            {
-              networking.hostName = hostname;
-            }
-          )
-          ./hosts/${hostname}
-        ];
+        if "livecd" == hostname then
+          [ ]
+        else
+          [
+            (
+              { ... }:
+              {
+                networking.hostName = hostname;
+              }
+            )
+            ./hosts/${hostname}
+          ];
 
       # Configuration of users
       userModules =
         users:
-        nixpkgs.lib.optional (users != null) [
-          (
-            { ... }:
-            {
-              users.users = nixpkgs.lib.genAttrs users (username: {
-                home = "/home/${username}";
-              });
-            }
-          )
-        ]
-        ++ map (username: ./users/${username}) users;
+        if null == users then
+          [ ]
+        else
+          [
+            (
+              { ... }:
+              {
+                users.users = nixpkgs.lib.genAttrs users (username: {
+                  home = "/home/${username}";
+                });
+              }
+            )
+          ]
+          ++ map (username: ./users/${username}) users;
 
       # Configuration of Home Manager
       homeManagerModules =
         specialArgs:
-        nixpkgs.lib.optional (specialArgs.host.users != null) [
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = specialArgs;
-              backupFileExtension = "backup";
-              sharedModules =
-                [ nix-flatpak.homeManagerModules.nix-flatpak ]
-                ++ nixpkgs.lib.optional (builtins.elem "plasma" specialArgs.host.env) plasma-manager.homeManagerModules.plasma-manager;
-              users = nixpkgs.lib.genAttrs specialArgs.host.users (username: import ./home/${username});
-            };
-          }
-          (
-            { config, ... }:
+        if null == specialArgs.host.users then
+          [ ]
+        else
+          [
+            home-manager.nixosModules.home-manager
             {
-              home-manager.extraSpecialArgs.sysCfg = config;
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = specialArgs;
+                backupFileExtension = "backup";
+                sharedModules =
+                  [ nix-flatpak.homeManagerModules.nix-flatpak ]
+                  ++ nixpkgs.lib.optional (builtins.elem "plasma" specialArgs.host.env) plasma-manager.homeManagerModules.plasma-manager;
+                users = nixpkgs.lib.genAttrs specialArgs.host.users (username: import ./home/${username});
+              };
             }
-          )
-        ];
+            (
+              { config, ... }:
+              {
+                home-manager.extraSpecialArgs.sysCfg = config;
+              }
+            )
+          ];
     in
     {
       nixosConfigurations = builtins.mapAttrs (
