b7e092de514c038286efdcc45184c30281ea168f
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-03 11:28:12 +0800

    change code structure

--- flake.nix
+++ flake.nix
@@ -41,154 +41,118 @@
       nix-flatpak,
       ...
     }:
-    {
-      nixosConfigurations =
+    let
+      # List of hosts
+      hosts =
         let
-          # List of hosts
-          hosts =
-            let
-              desktop-template = {
-                system = "x86_64-linux";
-                profile = "desktop";
-                env = [
-                  "plasma"
-                  "gaming"
-                  "obs"
-                  "virt-manager"
-                  "wine"
-                ];
-                users = [ "ccicnce113424" ];
-              };
-            in
-            {
-              ccic-desktop = desktop-template;
-              ccic-laptop = desktop-template;
-            };
-
-          # Configuration of Nix and Flake
-          nixConfigModules = [
-            ./nix-config.nix
-          ];
-
-          # Configuration of system
-          systemModules = system: [
-            ./system/${system}
-          ];
-
-          # Configuration of profile
-          profileModules = system: profile: [
-            ./profile/${system}/${profile}
-          ];
-
-          envModules = env: (map (env: ./env/${env}) env);
-
-          # Configuration of host
-          hostModules = hostname: [
-            (
-              { ... }:
-              {
-                networking.hostName = hostname;
-              }
-            )
-            ./hosts/${hostname}
-          ];
-
-          # Configuration of users
-          userModules =
-            users:
-            [
-              (
-                { ... }:
-                {
-                  users.users = nixpkgs.lib.genAttrs users (username: {
-                    home = "/home/${username}";
-                  });
-                }
-              )
-            ]
-            ++ map (username: ./users/${username}) users;
-
-          # Configuration of Home Manager
-          homeManagerModules = specialArgs: [
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = specialArgs;
-                backupFileExtension = "backup";
-                sharedModules =
-                  [ nix-flatpak.homeManagerModules.nix-flatpak ]
-                  ++ nixpkgs.lib.optional (builtins.elem "plasma" specialArgs.host.env) plasma-manager.homeManagerModules.plasma-manager;
-                users = nixpkgs.lib.genAttrs specialArgs.host.users (username: import ./home/${username});
-              };
-            }
-            (
-              { config, ... }:
-              {
-                home-manager.extraSpecialArgs.sysCfg = config;
-              }
-            )
-          ];
-        in
-        builtins.mapAttrs (
-          name: host:
-          nixpkgs.lib.nixosSystem rec {
-            specialArgs = {
-              inherit inputs host;
-            };
-            system = host.system;
-            modules =
-              [
-                nur.modules.nixos.default
-                daeuniverse.nixosModules.daed
-                nix-flatpak.nixosModules.nix-flatpak
-              ]
-              ++ nixConfigModules
-              ++ systemModules host.system
-              ++ profileModules host.system host.profile
-              ++ envModules host.env
-              ++ hostModules name
-              ++ userModules host.users
-              ++ homeManagerModules specialArgs;
-          }
-        ) hosts
-        // {
-          livecd = nixpkgs.lib.nixosSystem {
+          desktop-template = {
             system = "x86_64-linux";
-            modules = [
-              (
-                { pkgs, modulesPath, ... }:
-                {
-                  isoImage.squashfsCompression = "zstd";
-                  imports = [
-                    (modulesPath + "/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix")
-                    ./nix-config.nix
-                    daeuniverse.nixosModules.daed
-                  ];
-                  services.daed = {
-                    enable = true;
-                    openFirewall = {
-                      enable = true;
-                      port = 12345;
-                    };
-                    configDir = "/etc/daed";
-                    listen = "0.0.0.0:2023";
-                  };
-                  networking.firewall.allowedTCPPorts = [ 2023 ];
-                  environment.systemPackages = [
-                    pkgs.git
-                    pkgs.elinks
-                  ];
-                  services.openssh = {
-                    enable = true;
-                    settings.PermitRootLogin = "yes";
-                    openFirewall = true;
-                  };
-                }
-              )
+            profile = "desktop";
+            env = [
+              "plasma"
+              "gaming"
+              "obs"
+              "virt-manager"
+              "wine"
             ];
+            users = [ "ccicnce113424" ];
           };
+        in
+        {
+          ccic-desktop = desktop-template;
+          ccic-laptop = desktop-template;
         };
+
+      # Configuration of Nix and Flake
+      nixConfigModules = [
+        ./nix-config.nix
+      ];
+
+      # Configuration of system
+      systemModules = system: [
+        ./system/${system}
+      ];
+
+      # Configuration of profile
+      profileModules = system: profile: [
+        ./profile/${system}/${profile}
+      ];
+
+      envModules = env: (map (env: ./env/${env}) env);
+
+      # Configuration of host
+      hostModules = hostname: [
+        (
+          { ... }:
+          {
+            networking.hostName = hostname;
+          }
+        )
+        ./hosts/${hostname}
+      ];
+
+      # Configuration of users
+      userModules =
+        users:
+        [
+          (
+            { ... }:
+            {
+              users.users = nixpkgs.lib.genAttrs users (username: {
+                home = "/home/${username}";
+              });
+            }
+          )
+        ]
+        ++ map (username: ./users/${username}) users;
+
+      # Configuration of Home Manager
+      homeManagerModules = specialArgs: [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs;
+            backupFileExtension = "backup";
+            sharedModules =
+              [ nix-flatpak.homeManagerModules.nix-flatpak ]
+              ++ nixpkgs.lib.optional (builtins.elem "plasma" specialArgs.host.env) plasma-manager.homeManagerModules.plasma-manager;
+            users = nixpkgs.lib.genAttrs specialArgs.host.users (username: import ./home/${username});
+          };
+        }
+        (
+          { config, ... }:
+          {
+            home-manager.extraSpecialArgs.sysCfg = config;
+          }
+        )
+      ];
+    in
+    let
+      configGenerator =
+        name: host:
+        nixpkgs.lib.nixosSystem rec {
+          specialArgs = {
+            inherit inputs host;
+          };
+          system = host.system;
+          modules =
+            [
+              nur.modules.nixos.default
+              daeuniverse.nixosModules.daed
+              nix-flatpak.nixosModules.nix-flatpak
+            ]
+            ++ nixConfigModules
+            ++ systemModules host.system
+            ++ profileModules host.system host.profile
+            ++ envModules host.env
+            ++ hostModules name
+            ++ userModules host.users
+            ++ homeManagerModules specialArgs;
+        };
+    in
+    {
+      nixosConfigurations = builtins.mapAttrs configGenerator hosts // import ./nix-config.nix inputs;
     };
 }
