74a85d3294e87d54a3631058f53982e2c5265d10
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-06 22:08:14 +0800

    change obs settings

--- env/obs/default.nix
+++ env/obs/default.nix
@@ -1,20 +1,13 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
-  environment.systemPackages = [
-    (pkgs.wrapOBS {
-      plugins = with pkgs.obs-studio-plugins; [
-        obs-pipewire-audio-capture
-        obs-vkcapture
-        obs-vaapi
-        obs-gstreamer
-      ];
-    })
-  ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.kernelModules = [ "v4l2loopback" ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-pipewire-audio-capture
+      obs-vkcapture
+      obs-vaapi
+      obs-gstreamer
+    ];
+    enableVirtualCamera = true;
+  };
 }
