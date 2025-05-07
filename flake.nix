6aef593ab3bce521d77edb33255ff81efe704d0a
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-08 00:16:39 +0800

    fix lix

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
