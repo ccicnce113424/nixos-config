b7a9c2bde7c1c9a32622416ba4aeb06929e9ac14
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-09 01:30:11 +0800

    lix: use stable version

--- flake.nix
+++ flake.nix
@@ -5,14 +5,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
