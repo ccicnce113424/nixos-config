b21d55cbc160e131ab39a07ccc1c1541acb3c7ce
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-10 17:10:15 +0800

    change soundfont to arachno

--- home/ccicnce113424/multimedia/fluidsynth.nix
+++ home/ccicnce113424/multimedia/fluidsynth.nix
@@ -2,7 +2,7 @@
 {
   services.fluidsynth = {
     enable = true;
-    soundFont = "${pkgs.soundfont-generaluser}/share/soundfonts/GeneralUser-GS.sf2";
+    soundFont = "${pkgs.soundfont-arachno}/share/soundfonts/arachno.sf2";
     soundService = "pipewire-pulse";
   };
   home.packages = with pkgs; [ fluidsynth ];
