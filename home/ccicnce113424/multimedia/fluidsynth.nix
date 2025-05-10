e725ff887043a6705c804d4bbf2051ecf576bdbe
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-10 18:45:34 +0800

    test: add rosegarden

--- home/ccicnce113424/multimedia/fluidsynth.nix
+++ home/ccicnce113424/multimedia/fluidsynth.nix
@@ -5,6 +5,9 @@
     soundFont = "${pkgs.soundfont-arachno}/share/soundfonts/arachno.sf2";
     soundService = "pipewire-pulse";
   };
-  home.packages = with pkgs; [ fluidsynth ];
+  home.packages = with pkgs; [
+    fluidsynth
+    rosegarden
+  ];
   home.file.".local/share/soundfonts/default.sf2".source = config.services.fluidsynth.soundFont;
 }
