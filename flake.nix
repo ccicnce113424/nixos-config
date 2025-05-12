fdd43af00a7fef65dff14a8de44eaee84bc751f4
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-12 19:45:41 +0800

    keys are extra

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@ rec {
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
