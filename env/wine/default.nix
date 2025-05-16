241548a232fa58ac185de007a45af3463ff90267
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-17 01:26:32 +0800

    change ntsync udev rule

--- env/wine/default.nix
+++ env/wine/default.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
   boot.kernelModules = [ "ntsync" ];
-  services.udev.extraRules = ''KERNEL=="ntsync", MODE="0644"'';
+
+  # From: https://github.com/Cat-Lady/winesync-dkms/blob/main/99-ntsync.rules
+  services.udev.extraRules = ''KERNEL=="ntsync", MODE="0660", TAG+="uaccess"'';
   environment.systemPackages = with pkgs; [
     wineWowPackages.stagingFull
     winetricks
