cce0117f27b38a840b8aadc2958d548cd4ef3a3e
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-21 10:43:23 +0800

    add numtide cache

--- flake.nix
+++ flake.nix
@@ -64,10 +64,12 @@ rec {
     accept-flake-config = true;
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
       # "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
