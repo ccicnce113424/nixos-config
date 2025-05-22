a725cdde1c5a64aa08238a7a0ed67aa34cb5a6a0
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-22 19:18:34 +0800

    add nix-gaming cache

--- flake.nix
+++ flake.nix
@@ -61,11 +61,13 @@ rec {
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
+      "https://nix-gaming.cachix.org"
       # "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
