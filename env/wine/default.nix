29076b7451f2c132f05b1f89f347283ef4dfb392
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-27 13:08:37 +0800

    wine: fix winetricks

--- env/wine/default.nix
+++ env/wine/default.nix
@@ -1,10 +1,25 @@
-{ inputs, inputs', ... }:
+{
+  pkgs,
+  inputs,
+  inputs',
+  ...
+}:
+let
+  wine = inputs'.nix-gaming.packages.wine-tkg-ntsync;
+in
 {
   imports = [ inputs.nix-gaming.nixosModules.ntsync ];
   programs.wine.ntsync.enable = true;
-  environment.systemPackages = with inputs'.nix-gaming.packages; [
-    wine-tkg-ntsync
-    wineprefix-preparer
-    winetricks-git
-  ];
+  environment.systemPackages =
+    [
+      wine
+      (pkgs.runCommand "wine-alias" { buildInputs = [ wine ]; } ''
+        mkdir -p $out/bin
+        ln -s ${wine}/bin/wine $out/bin/wine64
+      '')
+    ]
+    ++ (with inputs'.nix-gaming.packages; [
+      wineprefix-preparer
+      winetricks-git
+    ]);
 }
