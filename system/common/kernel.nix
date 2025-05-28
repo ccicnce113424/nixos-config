f83f1af61e18e410ec17a18afa3eae34aaf86780
ccicnce113424 <ccicnce113424@gmail.com>
2025-05-29 02:06:49 +0800

    workaround: pin kernel version

--- system/common/kernel.nix
+++ system/common/kernel.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 {
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_14;
   boot.kernel.sysctl = {
     "kernel.sysrq" = 1;
   };
