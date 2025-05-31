5266bb08ac50c3475a259bab835cc7ebea710d90
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-01 00:28:17 +0800

    udev: fix printer

--- hosts/modules/common.nix
+++ hosts/modules/common.nix
@@ -65,7 +65,7 @@
         (pkgs.writeTextFile {
           name = "usb-rules";
           text = ''
-            SUBSYSTEM=="usb", MODE="0664", TAG+="uaccess", RUN+="${pkgs.acl}/bin/setfacl -m g:lp:rw $devpath"
+            SUBSYSTEM=="usb", MODE="0664", GROUP:="lp", TAG+="uaccess"
             SUBSYSTEM=="hidraw", MODE="0664", TAG+="uaccess"
           '';
           destination = "/etc/udev/rules.d/70-usb.rules";
