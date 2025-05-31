99cb63a153d05f81fa3d6a84028900c696a61154
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-01 01:33:49 +0800

    udev: remove wol settings for wireless connection

--- hosts/templates/desktop.nix
+++ hosts/templates/desktop.nix
@@ -13,7 +13,6 @@
 
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", NAME=="en*", RUN+="${pkgs.ethtool}/bin/ethtool -s $name wol g"
-    ACTION=="add", SUBSYSTEM=="net", NAME=="wl*", RUN+="${pkgs.ethtool}/bin/ethtool -s $name wol g"
   '';
 
   security.tpm2 = {
