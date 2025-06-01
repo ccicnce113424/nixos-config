8c0a574030460380f399dec4cb1befd1a0ee8db9
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-01 17:45:10 +0800

    linux: update to 6.15

--- system/common/kernel.nix
+++ system/common/kernel.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 {
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_14;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_15;
   boot.kernel.sysctl = {
     "kernel.sysrq" = 1;
   };
