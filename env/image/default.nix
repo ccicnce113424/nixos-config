76ecccfe3953e996c8dbe6e36b468ad1577acc3f
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-01 14:19:21 +0800

    switch to gimp3 and add xsane

---
+++ env/image/default.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, ... }:
+let
+  gimp-base = pkgs.gimp3;
+  xsane = pkgs.xsane.override {
+    gimpSupport = true;
+    gimp = gimp-base;
+  };
+  gimp-combined = pkgs.gimp3-with-plugins.override {
+    plugins =
+      lib.filter (
+        pkg: lib.isDerivation pkg && !pkg.meta.broken or false && pkgs.gimp3Plugins.lightning != pkg
+      ) (lib.attrValues pkgs.gimp3Plugins)
+      ++ [
+        (pkgs.runCommand "gimp-plugin-xsane" { buildInputs = [ xsane ]; } ''
+          mkdir -p $out/${gimp-base.targetPluginDir}/xsane
+          ln -s ${xsane}/bin/xsane $out/${gimp-base.targetPluginDir}/xsane
+        '')
+      ];
+  };
+in
+{
+  environment.systemPackages = [
+    gimp-combined
+    xsane
+  ];
+}
