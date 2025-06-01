5ec402698dac7805c191e63a52c07cb1625e9d10
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-01 13:49:44 +0800

    tpm and virt-manager enhancement

--- env/virt-manager/default.nix
+++ env/virt-manager/default.nix
@@ -1,5 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        vhostUserPackages = with pkgs; [ virtiofsd ];
+      };
+    };
+  };
   programs.virt-manager.enable = true;
 }
