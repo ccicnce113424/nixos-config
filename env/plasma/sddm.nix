eff3f85ef41478dd20e0f5b02c6aa24246c8cd74
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-01 13:28:39 +0800

    kde: simplify code

--- env/plasma/sddm.nix
+++ env/plasma/sddm.nix
@@ -1,10 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     wayland.compositor = "kwin";
   };
-
-  environment.systemPackages = with pkgs; [ kdePackages.sddm-kcm ];
 }
