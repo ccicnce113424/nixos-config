864a7cdc5f6aa58dcd1a1aa816fdb0cd920a0579
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-04 20:09:00 +0800

    switch to sjtu mirror to avoid evaluation error

--- hosts/modules/common.nix
+++ hosts/modules/common.nix
@@ -89,8 +89,8 @@
       i18n.defaultLocale = "zh_CN.UTF-8";
 
       nix.settings.substituters = [
-        "https://mirrors.ustc.edu.cn/nix-channels/store"
-        # "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
       ];
     })
