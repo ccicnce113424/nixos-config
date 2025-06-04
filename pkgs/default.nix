ed57eab4e76c1409fb61074d152a2055f195e7be
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-04 15:13:43 +0800

    remove workaround

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,6 @@
-{ pkgs, inputs', ... }:
+{ pkgs, ... }:
 {
   ccic-hello = pkgs.writeShellScriptBin "ccic-hello" "echo Hello, ccicnce113424!";
-
-  libplacebo = inputs'.nixpkgs-small.legacyPackages.libplacebo;
 
   shijima-qt = pkgs.callPackage ./shijima-qt { };
 }
