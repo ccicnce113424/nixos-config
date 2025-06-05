dc190461a81f30a05b29c401adff7e0add9af7e4
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-06 00:16:44 +0800

    disable microarch optimizaton

--- system/common/kernel.nix
+++ system/common/kernel.nix
@@ -1,11 +1,12 @@
 {
   pkgs,
-  config,
+  # config,
   lib,
   ...
 }:
 {
-  boot.kernelPackages = lib.mkForce pkgs.${config.pkgsarch}.linuxPackages_cachyos-lto;
+  # boot.kernelPackages = lib.mkForce pkgs.${config.pkgsarch}.linuxPackages_cachyos-lto;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos-lto;
   boot.kernel.sysctl = {
     "kernel.sysrq" = 1;
   };
