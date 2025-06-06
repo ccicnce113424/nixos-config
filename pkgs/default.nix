b580f6687a6f5dd62ce589c0b30d071be9f3396e
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-06 19:24:34 +0800

    uosc-danmaku: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
-{
+rec {
   ccic-hello = pkgs.writeShellScriptBin "ccic-hello" "echo Hello, ccicnce113424!";
 
   shijima-qt = pkgs.callPackage ./shijima-qt { };
   danmakufactory = pkgs.callPackage ./danmakufactory { };
+  uosc-danmaku = pkgs.callPackage ./uosc-danmaku {
+    inherit danmakufactory;
+    buildLua = pkgs.mpvScripts.buildLua;
+  };
 }
