7bfd4fd527baa3e92e42ce72c8a5667217888c1c
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-06 20:27:47 +0800

    fix mpv

--- home/ccicnce113424/multimedia/mpv/default.nix
+++ home/ccicnce113424/multimedia/mpv/default.nix
@@ -20,13 +20,13 @@
         ++ [
           pkgs.uosc-danmaku
         ];
-      extraMakeWrapperArgs = lib.optionals host.hostCfg.gpu.nvidia [
+      extraMakeWrapperArgs = lib.optionals host.hostCfg.gpu.nvidia or false [
         "--set"
         "ENABLE_HDR_WSI"
         "1"
       ];
     };
-    scriptOpts.uosc.controls = "menu,gap,<video,audio>subtitles,<has_many_audio>audio,<has_many_video>video,<has_many_edition>editions,<stream>stream-quality,button:danmaku,cycle:toggle_on:show_danmaku@uosc_danmaku:on=toggle_on/off=toggle_off?弹幕开关,gap,space,<video,audio>speed,space,shuffle,loop-playlist,loop-file,gap,prev,items,next,gap,fullscreen";
+    scriptOpts.uosc.controls = "menu,gap,<video,audio>subtitles,<has_many_audio>audio,<has_many_video>video,<has_many_edition>editions,<stream>stream-quality,button:danmaku,cycle:toggle_on:show_danmaku@uosc_danmaku:on=toggle_on/off=toggle_off?弹幕开关,button:danmaku_menu,gap,space,<video,audio>speed,space,shuffle,loop-playlist,loop-file,gap,prev,items,next,gap,fullscreen";
     config = {
       vo = "gpu-next";
       target-colorspace-hint = true;
