d1eaed3c0b5c4dffa1faa59f1adafca01d665106
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-09 13:35:12 +0800

    do not use easyoverlay

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -1,19 +1,21 @@
-{ self, inputs, ... }:
+{ self, ... }:
 {
-  imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
   perSystem =
     { pkgs, config, ... }:
     {
       packages = import ./default.nix { inherit pkgs; };
-      overlayAttrs = config.packages;
       apps.ccic-hello = {
         type = "app";
         program = "${config.packages.ccic-hello}/bin/ccic-hello";
       };
     };
-  flake.nixosModules.overlay =
-    { ... }:
-    {
-      nixpkgs.overlays = [ self.overlays.default ];
-    };
+
+  flake = {
+    overlays.default = (_: pkgs: import ./default.nix { inherit pkgs; });
+    nixosModules.overlay =
+      { ... }:
+      {
+        nixpkgs.overlays = [ self.overlays.default ];
+      };
+  };
 }
