7135699613f91d6921595b4828b7170d1ef31d1f
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-12 01:06:29 +0800

    fcitx5: use dicts from nixpkgs

--- env/plasma/fcitx5.nix
+++ env/plasma/fcitx5.nix
@@ -7,12 +7,15 @@
       plasma6Support = true;
       addons =
         with pkgs;
-        [ qt6Packages.fcitx5-chinese-addons ]
+        [
+          qt6Packages.fcitx5-chinese-addons
+          fcitx5-pinyin-moegirl
+          fcitx5-pinyin-zhwiki
+          fcitx5-pinyin-minecraft
+        ]
         ++ (with pkgs.nur.repos.aleksana; [
           fcitx5-pinyin-cedict
           fcitx5-pinyin-chinese-idiom
-          fcitx5-pinyin-moegirl
-          fcitx5-pinyin-zhwiki
         ]);
       settings.addons.classicui.globalSection = {
         Theme = "plasma";
