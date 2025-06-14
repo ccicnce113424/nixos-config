48e13046aabb6a40b17b48be117f8fd2303bc9bd
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-15 01:22:25 +0800

    Revert "do not use easyoverlay"
    This reverts commit d1eaed3c0b5c4dffa1faa59f1adafca01d665106.

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -1,21 +1,17 @@
-{ self, ... }:
+{ self, inputs, ... }:
 {
+  imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
   perSystem =
     { pkgs, config, ... }:
     {
       packages = import ./default.nix { inherit pkgs; };
+      overlayAttrs = config.packages;
       apps.ccic-hello = {
         type = "app";
         program = "${config.packages.ccic-hello}/bin/ccic-hello";
       };
     };
-
-  flake = {
-    overlays.default = (_: pkgs: import ./default.nix { inherit pkgs; });
-    nixosModules.overlay =
-      { ... }:
-      {
-        nixpkgs.overlays = [ self.overlays.default ];
-      };
+  flake.nixosModules.overlay = {
+    nixpkgs.overlays = [ self.overlays.default ];
   };
 }
