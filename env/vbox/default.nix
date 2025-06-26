c186b1eb19ca41f0f284c7b22fe1c99f14ca2db1
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-26 22:18:57 +0800

    vbox: fix libgl

--- env/vbox/default.nix
+++ env/vbox/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation.virtualbox.host = {
     enable = true;
@@ -6,5 +6,15 @@
     enableExtensionPack = true;
     addNetworkInterface = false;
     enableHardening = false;
+    # https://github.com/NixOS/nixpkgs/issues/382233#issuecomment-2888150127
+    # https://github.com/NixOS/nixpkgs/pull/409005
+    package = pkgs.virtualbox.overrideAttrs (oldAttrs: {
+      postPatch =
+        oldAttrs.postPatch
+        + ''
+          substituteInPlace src/VBox/Devices/Graphics/DevVGA-SVGA3d-glLdr.cpp \
+          --replace-fail '"libGL.so.1"' '"${pkgs.libGL}/lib/libGL.so.1"'
+        '';
+    });
   };
 }
