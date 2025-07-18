b9eabdc3b1019efe686f2d9c8f894e30153d1704
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-18 19:58:13 +0800

    virtualbox: apply patch

--- env/vbox/default.nix
+++ env/vbox/default.nix
@@ -1,4 +1,17 @@
 {
+  # https://github.com/NixOS/nixpkgs/issues/382233#issuecomment-2888150127
+  # https://github.com/NixOS/nixpkgs/pull/409005
+
+  pkgsPatch = [
+    (
+      p:
+      p.fetchpatch {
+        url = "https://github.com/NixOS/nixpkgs/pull/409005.patch";
+        hash = "sha256-akpVwrNMxItt5rD+S76HuOMK+NPYpz5tp+h5bB8VHN4=";
+      }
+    )
+  ];
+
   virtualisation.virtualbox.host = {
     enable = true;
     enableKvm = true;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,15 +1,4 @@
 { pkgs }:
 {
   ccic-hello = pkgs.writeShellScriptBin "ccic-hello" "echo Hello, ccicnce113424!";
-
-  # https://github.com/NixOS/nixpkgs/issues/382233#issuecomment-2888150127
-  # https://github.com/NixOS/nixpkgs/pull/409005
-  virtualbox = pkgs.virtualbox.overrideAttrs (oldAttrs: {
-    postPatch =
-      oldAttrs.postPatch
-      + ''
-        substituteInPlace src/VBox/Devices/Graphics/DevVGA-SVGA3d-glLdr.cpp \
-        --replace-fail '"libGL.so.1"' '"${pkgs.libGL}/lib/libGL.so.1"'
-      '';
-  });
 }
