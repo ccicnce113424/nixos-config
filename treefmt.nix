37c9f93938fbdd45400ea0e3cc5540de5b52a559
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-19 18:49:39 +0800

    treefmt: set priority

--- treefmt.nix
+++ treefmt.nix
@@ -6,12 +6,19 @@
     {
       treefmt = {
         projectRootFile = "flake.nix";
+        programs.deadnix = {
+          enable = true;
+          priority = 0;
+        };
+        programs.statix = {
+          enable = true;
+          priority = 1;
+        };
         programs.nixfmt = {
           enable = true;
           package = pkgs.nixfmt-rfc-style;
+          priority = 2;
         };
-        programs.deadnix.enable = true;
-        programs.statix.enable = true;
         programs.prettier.enable = true;
         programs.just.enable = true;
         programs.shfmt.enable = true;
