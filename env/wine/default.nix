c5ac3db84e640d2a253301672661d2ff8e9d5399
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-23 10:58:53 +0800

    wine: use new module

--- env/wine/default.nix
+++ env/wine/default.nix
@@ -3,23 +3,17 @@
   inputs,
   ...
 }:
-let
-  # from nix-gaming
-  wine = pkgs.wine-tkg-ntsync;
-in
 {
-  imports = [ inputs.nix-gaming.nixosModules.ntsync ];
-  programs.wine.ntsync.enable = true;
-  environment.systemPackages = [
-    wine
-  ]
-  ++ (with pkgs; [
+  imports = [ inputs.nix-gaming.nixosModules.wine ];
+  programs.wine = {
+    enable = true;
+    package = pkgs.wine-tkg-ntsync;
+    binfmt = true;
+    ntsync = true;
+  };
+  environment.systemPackages = with pkgs; [
     # following packages are from nix-gaming
     wineprefix-preparer
     winetricks-git
-  ]);
-
-  environment.sessionVariables = {
-    WINE_BIN = "/run/current-system/sw/bin/wine";
-  };
+  ];
 }
