d5effd8655b88affb9af337bee52d5901eea4827
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-28 15:07:40 +0800

    wine: remove patch

--- env/wine/default.nix
+++ env/wine/default.nix
@@ -4,16 +4,6 @@
   ...
 }:
 {
-  # https://github.com/NixOS/nixpkgs/issues/428236
-  pkgsPatch = [
-    (
-      p:
-      p.fetchpatch {
-        url = "https://github.com/ccicnce113424/nixpkgs/commit/cebd31d1e589d4a7aef270a46073b59b7cf44d82.patch";
-        hash = "sha256-viNjNXW1t1d3fVAdz+m5psJl1/cgR9POnJOsok10tME=";
-      }
-    )
-  ];
   imports = [ inputs.nix-gaming.nixosModules.wine ];
   programs.wine = {
     enable = true;
