cc9e94f1edf93551dba034a569255f312e12b29d
ccicnce113424 <ccicnce113424@gmail.com>
2025-08-03 23:54:21 +0800

    remove vbox patch

--- env/vbox/default.nix
+++ env/vbox/default.nix
@@ -1,16 +1,4 @@
 {
-  # https://github.com/NixOS/nixpkgs/issues/382233#issuecomment-2888150127
-  # TODO: remove this when https://github.com/NixOS/nixpkgs/pull/428689 is merged
-  pkgsPatch = [
-    (
-      p:
-      p.fetchpatch {
-        url = "https://github.com/NixOS/nixpkgs/commit/33853c9c64c6c1da9bc19fd0daed7bc29229250e.patch";
-        hash = "sha256-VIkTvsZdRWUt8VsTKBoHqXVVJSg59kzV+B8hhSFgub8=";
-      }
-    )
-  ];
-
   virtualisation.virtualbox.host = {
     enable = true;
     enableKvm = true;
