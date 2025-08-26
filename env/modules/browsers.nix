9809ecb5b477734e2f19aac8c00fd661b4b2962a
ccicnce113424 <ccicnce113424@gmail.com>
2025-08-26 20:36:03 +0800

    chromium: disable widevine

--- env/modules/browsers.nix
+++ env/modules/browsers.nix
@@ -41,4 +41,15 @@
       enableWideVine = true;
     })
   ];
+
+  pkgsPatch = [
+    (
+      p:
+      p.fetchpatch {
+        name = "widevine-fix.patch";
+        url = "https://github.com/NixOS/nixpkgs/commit/c577a4f0588042b6c6e30e9a2f267a6e3ebb97fe.patch";
+        hash = "sha256-tZII6j202sNImgp40mVjJqYeOAHQDqdqYEvn2PoYWW0=";
+      }
+    )
+  ];
 }
