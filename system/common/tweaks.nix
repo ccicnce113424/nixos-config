bb1bd6437aa29f6d2d67d01340e8c34c7a114f42
ccicnce113424 <ccicnce113424@gmail.com>
2025-08-26 21:00:07 +0800

    fix scx

--- system/common/tweaks.nix
+++ system/common/tweaks.nix
@@ -2,6 +2,16 @@
 {
   services.dbus.implementation = "broker";
   services.scx.enable = true;
+  pkgsPatch = [
+    (
+      p:
+      p.fetchpatch {
+        name = "fix-scx.patch";
+        url = "https://github.com/NixOS/nixpkgs/commit/15f47f87239d31a84da0e148042e283de6a43f65.patch";
+        hash = "sha256-qMSLkuDQvAFSBxW4E1hRDWEmcXdbpI+b+xpQ/mbhwEw=";
+      }
+    )
+  ];
 
   environment.systemPackages = with pkgs; [
     uutils-coreutils-noprefix
