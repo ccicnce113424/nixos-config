4271368b0015eb2fd30215fb8de36c4933a94d04
ccicnce113424 <ccicnce113424@gmail.com>
2025-09-22 00:38:43 +0800

    vscode: add makefile extension

--- home/ccicnce113424/development/c.nix
+++ home/ccicnce113424/development/c.nix
@@ -4,6 +4,7 @@
     profiles.default.extensions = with pkgs.vscode-extensions; [
       llvm-vs-code-extensions.vscode-clangd
       vadimcn.vscode-lldb
+      ms-vscode.makefile-tools
     ];
   };
 }
