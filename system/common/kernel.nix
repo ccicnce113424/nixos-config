23b198c5b0bf25bf5f551c8649fb8cfcb9c35622
ccicnce113424 <ccicnce113424@gmail.com>
2025-09-21 22:38:50 +0800

    use gcc kernel

--- system/common/kernel.nix
+++ system/common/kernel.nix
@@ -6,7 +6,7 @@
 }:
 {
   # boot.kernelPackages = lib.mkForce pkgs.${config.pkgsArch}.linuxPackages_cachyos-lto;
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos-lto;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos-gcc;
   boot.kernel.sysctl = {
     "kernel.sysrq" = 1;
   };
