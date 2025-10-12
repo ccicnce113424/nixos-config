6d096da83d9eaef7862c1553c109d23c95c211ff
ccicnce113424 <ccicnce113424@gmail.com>
2025-10-12 15:12:11 +0800

    vbox: fix

--- env/vbox/default.nix
+++ env/vbox/default.nix
@@ -11,6 +11,14 @@
     (
       p:
       p.fetchpatch {
+        name = "vbox-curl-fix.patch";
+        url = "https://github.com/NixOS/nixpkgs/pull/450867.patch";
+        hash = "sha256-C9ljliH1w1vOXPPggciN0QaH4NufTZn8pdiCXfSZXzg=";
+      }
+    )
+    (
+      p:
+      p.fetchpatch {
         name = "vbox-fix-3d-acceleration.patch";
         url = "https://github.com/NixOS/nixpkgs/pull/451054.patch";
         hash = "sha256-BID9s7WL50EQyF6ckhiAjBetd3uNMi7eG5XdLKlS8CU=";
