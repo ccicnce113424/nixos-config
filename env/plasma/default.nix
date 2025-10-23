3b1054c606bfce6faceb87142479f4e92158a2e9
ccicnce113424 <ccicnce113424@gmail.com>
2025-10-23 17:00:00 +0800

    fix cantata

--- env/plasma/default.nix
+++ env/plasma/default.nix
@@ -19,5 +19,14 @@
         hash = "sha256-e7RLeDzAdbThxO0ytNtfa6dsE/oy0zhEUF52Yoy07eU=";
       }
     )
+
+    (
+      p:
+      p.fetchpatch2 {
+        name = "fix-cantata-qt-610-build.patch";
+        url = "https://github.com/NixOS/nixpkgs/pull/454827.patch";
+        hash = "sha256-dLDUBVkt7bpFwuwAUEG6fbpbgAp+kKU9LNaXE78ncqQ=";
+      }
+    )
   ];
 }
