29419f7c5cf7b18e43992a6db95f3f6e77e50405
ccicnce113424 <ccicnce113424@gmail.com>
2025-10-26 17:02:51 +0800

    remove patch

--- env/plasma/default.nix
+++ env/plasma/default.nix
@@ -14,25 +14,9 @@
     (
       p:
       p.fetchpatch {
-        name = "fix-nheko-qt-610-build.patch";
-        url = "https://github.com/NixOS/nixpkgs/pull/454781.patch";
-        hash = "sha256-ESYlsWVdL/qMMiGG8Y9so9LUZPF9bFZ6lunjNzWB+r8=";
-      }
-    )
-    (
-      p:
-      p.fetchpatch {
         name = "fix-cantata-qt-610-build.patch";
         url = "https://github.com/NixOS/nixpkgs/pull/454827.patch";
         hash = "sha256-CK+m1+qJ2GT4YnRVwEKUtstVcZhI5JCEjIhYkCgh5iw=";
-      }
-    )
-    (
-      p:
-      p.fetchpatch {
-        name = "fix-ayugram-qt-610-build.patch";
-        url = "https://github.com/NixOS/nixpkgs/pull/454863.patch";
-        hash = "sha256-oGulmK++tnICqzKuki5/Dk0AcPlTVOTqvX/HZeUsR6M=";
       }
     )
   ];
