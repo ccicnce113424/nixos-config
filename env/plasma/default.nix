f3003b8be9ab1d613fda6d76f38788c5bf2d81f6
ccicnce113424 <ccicnce113424@gmail.com>
2025-10-30 15:10:14 +0800

    remove patch

--- env/plasma/default.nix
+++ env/plasma/default.nix
@@ -9,15 +9,4 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
-
-  pkgsPatch = [
-    (
-      p:
-      p.fetchpatch {
-        name = "fix-cantata-qt-610-build.patch";
-        url = "https://github.com/NixOS/nixpkgs/pull/454827.patch";
-        hash = "sha256-CK+m1+qJ2GT4YnRVwEKUtstVcZhI5JCEjIhYkCgh5iw=";
-      }
-    )
-  ];
 }
