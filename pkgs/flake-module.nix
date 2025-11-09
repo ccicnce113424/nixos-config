e733ae1e36a70a607591d585983af426c338b937
ccicnce113424 <ccicnce113424@gmail.com>
2025-11-10 00:59:54 +0800

    ci: fix vbox builder

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -2,7 +2,12 @@
 {
   imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
   perSystem =
-    { pkgs, config, ... }:
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
     {
       legacyPackages = import ./default.nix { inherit pkgs; };
       overlayAttrs = config.legacyPackages;
@@ -10,6 +15,9 @@
         type = "app";
         program = "${config.legacyPackages.ccic-hello}/bin/ccic-hello";
       };
+      packages.vbox = lib.findFirst (
+        p: lib.hasPrefix "virtualbox-" p.name
+      ) null self.nixosConfigurations.ccic-desktop.config.environment.systemPackages;
     };
   flake.nixosModules.overlay = {
     nixpkgs.overlays = [ self.overlays.default ];
