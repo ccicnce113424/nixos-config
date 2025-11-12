fb5ec50e688e77273247e2bbfff16f031541679f
ccicnce113424 <ccicnce113424@gmail.com>
2025-11-13 00:11:17 +0800

    remove patch of nom

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,14 +1,4 @@
 { pkgs }:
 {
   ccic-hello = pkgs.writeShellScriptBin "ccic-hello" "echo Hello, ccicnce113424!";
-
-  nix-output-monitor = pkgs.nix-output-monitor.overrideAttrs (prev: {
-    patches = prev.patches or [ ] ++ [
-      (pkgs.fetchpatch2 {
-        name = "fix-local-store-url-parsing.patch";
-        url = "https://github.com/maralorn/nix-output-monitor/pull/203.patch";
-        hash = "sha256-unpJ+tZO2HLVion7vvhj+Xn2wFOzwxnqMohPIFACX+Q=";
-      })
-    ];
-  });
 }
