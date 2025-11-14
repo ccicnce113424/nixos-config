1abd68237e674a97251d89f87d3a0a812dde6594
ccicnce113424 <ccicnce113424@gmail.com>
2025-11-15 05:24:26 +0800

    firefox: do not use binary

--- env/modules/browsers.nix
+++ env/modules/browsers.nix
@@ -2,7 +2,7 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-bin;
+    # package = pkgs.firefox-bin;
     languagePacks = { CN = [ "zh-CN" ]; }.${config.hostCfg.locale};
     preferences = {
       # Settings for integration
