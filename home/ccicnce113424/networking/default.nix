d3b51beffe491f11b5e8de728ce884ceab57ff49
ccicnce113424 <ccicnce113424@gmail.com>
2025-11-19 22:06:32 +0800

    wrap ayugram

--- home/ccicnce113424/networking/default.nix
+++ home/ccicnce113424/networking/default.nix
@@ -1,18 +1,30 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    nur.repos.lonerOrz.qq
-    wechat
-    wemeet
-    ayugram-desktop
-    tor-browser
-    qbittorrent-enhanced
-    motrix
-    nheko
-    cherry-studio
+  pkgs,
+  lib,
+  host,
+  ...
+}:
+{
+  home.packages =
+    (with pkgs; [
+      nur.repos.lonerOrz.qq
+      wechat
+      wemeet
+      ayugram-desktop
+      tor-browser
+      qbittorrent-enhanced
+      motrix
+      nheko
+      cherry-studio
 
-    nur.repos.xddxdd.peerbanhelper
-  ];
+      nur.repos.xddxdd.peerbanhelper
+    ])
+    # https://github.com/NixOS/nixpkgs/issues/424868
+    ++ lib.optional host.hostCfg.gpu.nvidia or false (
+      pkgs.writeShellScriptBin "AyuGram" ''
+        WEBKIT_DISABLE_DMABUF_RENDERER=1 exec ${pkgs.ayugram-desktop}/bin/AyuGram "$@"
+      ''
+    );
 
   imports = [
     ./firefox.nix
