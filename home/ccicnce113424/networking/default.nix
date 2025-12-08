441876138ec279786987326729e5d5ccfeac5831
ccicnce113424 <ccicnce113424@gmail.com>
2025-12-08 13:02:09 +0800

    remove webkit disable dmabuf wrap

--- home/ccicnce113424/networking/default.nix
+++ home/ccicnce113424/networking/default.nix
@@ -1,47 +1,21 @@
 {
   pkgs,
-  lib,
-  host,
   ...
 }:
 {
-  home.packages =
-    let
-      nvFixWebkitGtk = map (
-        pkg:
-        let
-          exe = lib.getExe pkg;
-          basename = builtins.baseNameOf exe;
-          wrapper = pkgs.writeShellScriptBin basename ''
-            WEBKIT_DISABLE_DMABUF_RENDERER=1 exec ${exe} "$@"
-          '';
-        in
-        lib.hiPrio wrapper
-      );
-    in
-    (with pkgs; [
-      nur.repos.lonerOrz.qq
-      wechat
-      wemeet
-      ayugram-desktop
-      tor-browser
-      qbittorrent-enhanced
-      imfile
-      nheko
-      cherry-studio
+  home.packages = with pkgs; [
+    nur.repos.lonerOrz.qq
+    wechat
+    wemeet
+    ayugram-desktop
+    tor-browser
+    qbittorrent-enhanced
+    imfile
+    nheko
+    cherry-studio
 
-      nur.repos.xddxdd.peerbanhelper
-    ])
-    # https://github.com/NixOS/nixpkgs/issues/424868
-    ++ lib.optionals host.hostCfg.gpu.nvidia or false (
-      nvFixWebkitGtk (
-        with pkgs;
-        [
-          wechat
-          ayugram-desktop
-        ]
-      )
-    );
+    nur.repos.xddxdd.peerbanhelper
+  ];
 
   imports = [
     ./firefox.nix
