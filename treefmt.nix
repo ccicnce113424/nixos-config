cc89b952c514cc108f7ad3727d4e13a7b43ab74e
ccicnce113424 <ccicnce113424@gmail.com>
2026-01-04 14:15:24 +0800

    treefmt: update

--- treefmt.nix
+++ treefmt.nix
@@ -1,21 +1,31 @@
 { inputs, ... }:
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
-  perSystem = {
-    treefmt = {
-      projectRootFile = "flake.nix";
-      programs.nixfmt = {
-        enable = true;
-        priority = 0;
+  perSystem =
+    { ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs.nixfmt = {
+          enable = true;
+          priority = 0;
+        };
+        programs.deadnix = {
+          enable = true;
+          priority = 1;
+        };
+        programs.nixf-diagnose = {
+          enable = true;
+          priority = 2;
+        };
+        settings.formatter.nixf-diagnose.options = [
+          "--auto-fix"
+          "--ignore"
+          "sema-primop-overridden"
+        ];
+        programs.prettier.enable = true;
+        programs.just.enable = true;
+        programs.shfmt.enable = true;
       };
-      programs.nixf-diagnose = {
-        enable = true;
-        priority = 1;
-      };
-      settings.formatter.nixf-diagnose.options = [ "--auto-fix" ];
-      programs.prettier.enable = true;
-      programs.just.enable = true;
-      programs.shfmt.enable = true;
     };
-  };
 }
