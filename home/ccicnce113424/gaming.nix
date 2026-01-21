2af33c4985e63a7294e43a9d09e66623755ed457
ccicnce113424 <ccicnce113424@gmail.com>
2026-01-21 10:13:58 +0800

    discord: don't use hm module

--- home/ccicnce113424/gaming.nix
+++ home/ccicnce113424/gaming.nix
@@ -15,13 +15,9 @@
     winePackages = [ osConfig.programs.wine.package ];
   };
 
-  programs.discord = {
-    enable = true;
-    package = pkgs.discord-krisp.override {
+  home.packages = [
+    (pkgs.discord-krisp.override {
       withOpenASAR = true;
-    };
-    settings = {
-      DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
-    };
-  };
+    })
+  ];
 }
