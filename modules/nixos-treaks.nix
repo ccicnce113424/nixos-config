acc6dae11d061a0a0ef4e8a43f098762433b82df
ccicnce113424 <ccicnce113424@gmail.com>
2026-02-17 12:13:04 +0800

    remove dead code and fix cmd alias

--- modules/nixos-treaks.nix
+++ modules/nixos-treaks.nix
@@ -6,34 +6,6 @@
   ...
 }:
 let
-  # aliasPkg =
-  #   {
-  #     name,
-  #     text,
-  #     stdenvNoCC,
-  #     writeShellScriptBin,
-  #     copyDesktopItems,
-  #     makeDesktopItem,
-  #   }:
-  #   stdenvNoCC.mkDerivation {
-  #     name = "${name}-cmdalias";
-  #     src = writeShellScriptBin name text;
-  #     nativeBuildInputs = [ copyDesktopItems ];
-  #     desktopItems = [
-  #       (makeDesktopItem {
-  #         inherit name;
-  #         desktopName = name;
-  #         exec = name;
-  #         terminal = true;
-  #         comment = "run \"${name}\"";
-  #       })
-  #     ];
-  #     installPhase = ''
-  #       runHook preInstall
-  #       install -D bin/${name} $out/bin/${name}
-  #       runHook postInstall
-  #     '';
-  #   };
   cfg = {
     environment.systemPackages =
       with pkgs;
@@ -41,9 +13,6 @@ let
         git
         ccic-hello
       ]
-      # ++ lib.mapAttrsToList (
-      #   name: text: pkgs.callPackage aliasPkg { inherit name text; }
-      # ) config.cmdAlias;
       ++ lib.mapAttrsToList writeShellScriptBin config.cmdAlias;
 
     nix.settings = nixConfig;
@@ -63,27 +32,27 @@ in
       # Commands to add
       switch = ''
         rm -f $HOME/.config/fontconfig/conf.d/10-hm-fonts.conf.backup
-        systemd-inhibit nh os switch $@
+        systemd-inhibit nh os switch "$@"
       '';
       cswitch = ''
         rm -f $HOME/.config/fontconfig/conf.d/10-hm-fonts.conf.backup
-        systemd-inhibit sudo nixos-rebuild switch --flake $HOME/code/nixos-config --install-bootloader --option substituters 'https://cache.nixos.org' $@
+        systemd-inhibit sudo nixos-rebuild switch --flake $HOME/code/nixos-config --install-bootloader --option substituters 'https://cache.nixos.org' "$@"
       '';
-      sgc = "systemd-inhibit nix store gc $@";
+      sgc = ''systemd-inhibit nix store gc "$@"'';
       up = ''
         set -e
         cd $HOME/code/nixos-config
         systemd-inhibit git pull
-        switch $@
+        switch "$@"
       '';
-      pclean = "systemd-inhibit nh clean all $@";
+      pclean = ''systemd-inhibit nh clean all "$@"'';
       clr = ''
         set -e
         pclean
-        switch $@
+        switch "$@"
       '';
-      win = "systemctl reboot --boot-loader-entry=auto-windows $@";
-      fw = "systemctl reboot --firmware-setup $@";
+      win = ''systemctl reboot --boot-loader-entry=auto-windows "$@"'';
+      fw = ''systemctl reboot --firmware-setup "$@"'';
     };
   };
   options.enable32Bit = lib.mkEnableOption "32-bit dependencies";
