ada258d1c6cf292307dbb432b4c8eb09915d5686
ccicnce113424 <ccicnce113424@gmail.com>
2026-03-02 12:59:16 +0800

    fix nvidia

--- modules/genpkg.nix
+++ modules/genpkg.nix
@@ -21,6 +21,10 @@ let
       url = "https://github.com/NixOS/nixpkgs/pull/489469.patch";
       hash = "sha256-YURNV8eaBNcrl9+nwaQlg2QANlCluuxVQcxmfWptWBg=";
     }
+    {
+      url = "https://github.com/NixOS/nixpkgs/pull/495704.patch";
+      hash = "sha256-+QbtuX4rGVnitTfj7ot7KIheTLkmXHMAmGyvfJ76Bko=";
+    }
   ];
   replaceModules = [
     # 489469
