9f7318a1060609b4c39b212a5f6601eb5d76365b
ccicnce113424 <ccicnce113424@gmail.com>
2026-03-11 14:34:25 +0800

    update hash

--- modules/genpkg.nix
+++ modules/genpkg.nix
@@ -22,7 +22,7 @@ let
     }
     {
       url = "https://github.com/NixOS/nixpkgs/pull/498612.diff?full_index=1";
-      hash = "sha256-7YfykZQCD2TUNyG230C8Ua+U9Czq74UOB70gk8OIzek=";
+      hash = "sha256-Cr0lcnDEujl6fqLh6imQYXSsUOcW/zVhdKh3UuArW38=";
     }
   ];
   replaceModules = [
