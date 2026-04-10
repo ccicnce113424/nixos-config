b2cfb96efb78c31d50973d0cd1cb4e3444a2f295
ccicnce113424 <ccicnce113424@gmail.com>
2026-04-11 05:07:43 +0800

    mpv: fix vp9

--- home/ccicnce113424/multimedia/mpv/default.nix
+++ home/ccicnce113424/multimedia/mpv/default.nix
@@ -18,11 +18,16 @@
         ++ [
           pkgs.uosc-danmaku-git
         ];
-      # extraMakeWrapperArgs = lib.optionals host.hostCfg.gpu.nvidia or false [
-      #   "--set"
-      #   "ENABLE_HDR_WSI"
-      #   "1"
-      # ];
+      mpv-unwrapped = pkgs.mpv-unwrapped.override {
+        ffmpeg = pkgs.ffmpeg.overrideAttrs (old: {
+          patches = (old.patches or [ ]) ++ [
+            (pkgs.fetchpatch2 {
+              url = "https://code.ffmpeg.org/FFmpeg/FFmpeg/commit/672635932684c0ee7cfbb7f9eef6999b4e72df4b.patch";
+              hash = "sha256-qT+ghGwpW9feV9aMVbc9pIAo4UR8qAX+M1u4EByV/tY=";
+            })
+          ];
+        });
+      };
     };
     scriptOpts.uosc.controls = "menu,gap,<video,audio>subtitles,<has_many_audio>audio,<has_many_video>video,<has_many_edition>editions,<stream>stream-quality,button:danmaku,cycle:toggle_on:show_danmaku@uosc_danmaku:on=toggle_on/off=toggle_off?弹幕开关,button:danmaku_menu,gap,space,<video,audio>speed,space,shuffle,loop-playlist,loop-file,gap,prev,items,next,gap,fullscreen";
     config = {
