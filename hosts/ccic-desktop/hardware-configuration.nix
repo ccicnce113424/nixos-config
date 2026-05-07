d584849b9ac7b7a529ba38ae8b52ff8ede3be9c4
ccicnce113424 <ccicnce113424@gmail.com>
2026-05-07 19:27:10 +0800

    Revert "use r8125 driver"
    This reverts commit 2385cb67d8dc70949b162d678a6f4eeb289259f0.

--- hosts/ccic-desktop/hardware-configuration.nix
+++ hosts/ccic-desktop/hardware-configuration.nix
@@ -22,8 +22,7 @@
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ r8125 ];
-  boot.blacklistedKernelModules = [ "r8169" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/6c9d02f5-f6b1-414a-8e47-8a73216b8f72";
