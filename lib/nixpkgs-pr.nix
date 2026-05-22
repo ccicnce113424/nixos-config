a537abafc41a196b07178667447fb68d6a8a6976
ccicnce113424 <ccicnce113424@gmail.com>
2026-05-23 06:48:32 +0800

    fix tpm

--- lib/nixpkgs-pr.nix
+++ lib/nixpkgs-pr.nix
@@ -17,6 +17,9 @@ in
       # kmscon: 9.3.5 -> 9.3.5-2026-05-15
       # test upcoming kmscon update
       520693
+      # python3Packages.tpm2-pytss: fix build with cryptography 47
+      # Merged into upstream, but not yet in nixos-unstable channel
+      522374
     ];
   };
 
