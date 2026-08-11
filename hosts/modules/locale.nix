4a0f89491408f3eb35e5ed6156e647f18f30897e
ccicnce113424 <ccicnce113424@gmail.com>
2026-08-11 12:42:04 +0800

    add ustc substituter

--- hosts/modules/locale.nix
+++ hosts/modules/locale.nix
@@ -21,7 +21,7 @@ let
             priority = 35;
           })
           [
-            # "https://mirrors.ustc.edu.cn/nix-channels/store"
+            "https://mirrors.ustc.edu.cn/nix-channels/store"
             # "https://mirror.sjtu.edu.cn/nix-channels/store"
             # "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
             "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
