7974ae0f349f0ff756d96861676e4bd559653cb7
ccicnce113424 <ccicnce113424@gmail.com>
2026-08-11 23:05:55 +0800

    simplify findPkgs

--- lib/lib.nix
+++ lib/lib.nix
@@ -8,17 +8,14 @@
       default =
         names: pkgsList:
         let
-          nameSet = lib.genAttrs names (_: null);
-          getPackageName =
-            p:
-            p.pname or (builtins.parseDrvName (
-              p.name or (builtins.substring 33 (-1) (baseNameOf (builtins.unsafeDiscardStringContext p)))
-            )).name;
-          filtered = builtins.filter (p: builtins.hasAttr (getPackageName p) nameSet) pkgsList;
-          groups = builtins.groupBy getPackageName filtered;
+          # nameSet = lib.genAttrs names (_: null);
+          # matching = builtins.filter (p: builtins.hasAttr (lib.getName p) nameSet) pkgsList;
+          matching = builtins.filter (p: builtins.elem (lib.getName p) names) pkgsList;
+          groups = builtins.groupBy lib.getName matching;
         in
-        assert builtins.attrNames nameSet == builtins.attrNames groups;
-        builtins.mapAttrs (_name: pkgs: builtins.head pkgs) groups;
+        # assert builtins.attrNames nameSet == builtins.attrNames groups;
+        assert builtins.sort builtins.lessThan names == builtins.attrNames groups;
+        builtins.mapAttrs (_name: builtins.head) groups;
     };
     pathToPatchFileset = lib.mkOption {
       default =
