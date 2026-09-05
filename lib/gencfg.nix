8c7cee36f7db51b18a0e7caea27c1914c0bcbfec
ccicnce113424 <ccicnce113424@gmail.com>
2026-09-06 00:46:22 +0800

    set profile label to repo revision

--- lib/gencfg.nix
+++ lib/gencfg.nix
@@ -10,62 +10,59 @@
 {
   options.lib'.genOSConfig = lib.mkOption {
     default =
-      let
-        sharedModules = [
-          ../hosts/runtime.nix
-        ];
-
-        genOSConfig =
-          hosts:
-          builtins.mapAttrs (
-            name: host:
-            (withSystem host.system (
+      hosts:
+      builtins.mapAttrs (
+        name: host:
+        (withSystem host.system (
+          {
+            inputs',
+            ...
+          }:
+          let
+            patched = config.lib'.patchedNixpkgs host;
+            sharedModules = [
+              ../hosts/runtime.nix
               {
-                inputs',
-                ...
-              }:
-              let
-                patched = config.lib'.patchedNixpkgs host;
-                patchedRegistryModule = {
-                  nix.registry = {
-                    nixpkgs-patched.to = {
-                      type = "path";
-                      path = patched.finalNixpkgs.outPath;
-                    };
-                    mypkgs.to = {
-                      type = "path";
-                      path = inputs.nix-packages.outPath;
-                    };
-                    nixos-config.to = {
-                      type = "path";
-                      path = ./..;
-                    };
+                nix.registry = {
+                  nixpkgs-patched.to = {
+                    type = "path";
+                    path = patched.finalNixpkgs.outPath;
+                  };
+                  mypkgs.to = {
+                    type = "path";
+                    path = inputs.nix-packages.outPath;
+                  };
+                  nixos-config.to = {
+                    type = "path";
+                    path = ./..;
                   };
                 };
-              in
-              (import (patched.finalNixpkgs.outPath + "/nixos/lib/eval-config.nix")) {
-                lib = inputs.nixpkgs.lib;
-                inherit (host) system;
-                modules = sharedModules ++ [
-                  patchedRegistryModule
-                  ../hosts/${name}
-                ];
-                pkgs = patched.hostPkgs;
-                specialArgs = {
-                  inherit
-                    self
-                    inputs
-                    inputs'
-                    nixConfig
-                    ;
-                  host = host // {
-                    hostname = name;
-                  };
-                };
+                system.configurationRevision = self.rev or self.dirtyRev;
+                system.nixos.label = "${inputs.nixpkgs.shortRev}.${builtins.substring 0 8 self.lastModifiedDate}.${
+                  self.shortRev or self.dirtyShortRev
+                }";
               }
-            ))
-          ) hosts;
-      in
-      genOSConfig;
+            ];
+          in
+          (import (patched.finalNixpkgs.outPath + "/nixos/lib/eval-config.nix")) {
+            inherit (host) system;
+            modules = sharedModules ++ [
+              ../hosts/${name}
+            ];
+            pkgs = patched.hostPkgs;
+            specialArgs = {
+              inherit
+                self
+                inputs
+                inputs'
+                nixConfig
+                ;
+              host = host // {
+                hostname = name;
+              };
+            };
+          }
+        ))
+      ) hosts;
   };
 }
