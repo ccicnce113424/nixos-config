a6c883969e635e50d499376f172395e5652d0ce8
ccicnce113424 <ccicnce113424@gmail.com>
2026-09-06 11:55:13 +0800

    fix

--- env/image/default.nix
+++ env/image/default.nix
@@ -11,19 +11,15 @@ let
     gimp = gimp-base;
   };
   gimp-combined = pkgs.gimp3-with-plugins.override {
-    plugins =
-      lib.filter (pkg: lib.isDerivation pkg && !pkg.meta.broken or false) (
-        lib.attrValues pkgs.gimp3Plugins
-      )
-      ++ [
-        # (pkgs.runCommand "gimp-plugin-xsane" { buildInputs = [ xsane ]; } ''
-        #   mkdir -p $out/${gimp-base.targetPluginDir}/xsane
-        #   ln -s ${lib.getExe xsane} $out/${gimp-base.targetPluginDir}/xsane
-        # '')
-        (pkgs.linkFarm "gimp-plugin-xsane" {
-          "${gimp-base.targetPluginDir}/xsane" = lib.getExe xsane;
-        })
-      ];
+    plugins = [
+      # (pkgs.runCommand "gimp-plugin-xsane" { buildInputs = [ xsane ]; } ''
+      #   mkdir -p $out/${gimp-base.targetPluginDir}/xsane
+      #   ln -s ${lib.getExe xsane} $out/${gimp-base.targetPluginDir}/xsane
+      # '')
+      (pkgs.linkFarm "gimp-plugin-xsane" {
+        "${gimp-base.targetPluginDir}/xsane" = lib.getExe xsane;
+      })
+    ];
   };
 in
 {
