630be1bba568800a8bd94c2c20114dfcc8788b5c
ccicnce113424 <ccicnce113424@gmail.com>
2026-09-10 22:51:21 +0800

    Revert "re-enable lyrica"
    This reverts commit 9497a0c9430485ac75f26daef61add6f5a366609.

--- home/ccicnce113424/plasma/default.nix
+++ home/ccicnce113424/plasma/default.nix
@@ -17,15 +17,15 @@
           "org.kde.plasma.pager"
           "org.kde.plasma.icontasks"
           "org.kde.plasma.marginsseparator"
-          {
-            name = pkgs.lyrica.passthru.id;
-            config = {
-              Backend = {
-                tlyricMode = 3;
-              };
-              Frontend.shouldUseDefaultThemeTextColor = false;
-            };
-          }
+          # {
+          #   name = pkgs.lyrica.passthru.id;
+          #   config = {
+          #     Backend = {
+          #       tlyricMode = 3;
+          #     };
+          #     Frontend.shouldUseDefaultThemeTextColor = false;
+          #   };
+          # }
           # {
           #   name = "plasmusic-toolbar";
           #   config.General = {
@@ -97,7 +97,7 @@
   home.packages =
     with pkgs;
     [
-      lyrica
+      # lyrica
       kurve
       plasmusic-toolbar
       kdePackages.wallpaper-engine-plugin
